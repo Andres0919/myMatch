4000cb17299467b06e74cf2a3600e866273f56eb
Andrés Posada <andres.posada0919@gmail.com>
2020-06-09 20:51:09 -0500

    added hover color menu and background nav

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="fixed w-full">
-    <div class="flex justify-between bg-gray-200">
+    <div class="flex justify-between bg-white">
       <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 my-2 ml-6">
         MyMatch
       </div>
-      <div class="flex justify-between bg-gray-400 px-4 py-2 my-2">
-        <div class="text-gray-700 text-center bg-gray-200 px-4 mx-2">
-          1
+      <div class="flex justify-between bg-white px-4 py-2 my-2">
+        <div
+          class="text-gray-700 text-center bg-white px-4 mx-2 hover:underline hover:text-red-800 cursor-pointer"
+        >
+          INICIO
         </div>
-        <div class="text-gray-700 text-center bg-gray-200 px-4 mx-2">
-          2
+        <div
+          class="text-gray-700 text-center bg-white px-4 mx-2 hover:underline hover:text-red-800 cursor-pointer"
+        >
+          BLOG
         </div>
-        <div class="text-gray-700 text-center bg-gray-200 px-4 mx-2">
-          3
+        <div
+          class="text-gray-700 text-center bg-white px-4 mx-2 hover:underline hover:text-red-800 cursor-pointer"
+        >
+          VIDEOS
+        </div>
+        <div
+          class="text-gray-700 text-center bg-white px-4 mx-2 hover:underline hover:text-red-800 cursor-pointer"
+        >
+          CONTACTO
         </div>
       </div>
       <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 my-2 mr-6">
